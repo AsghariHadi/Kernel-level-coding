0ee221b023537595aaf22bf174279f6f0eb80223
Hadi Asghari <hadi.asgharime@gmail.com>
2023-06-10 07:55:29 +0200

    Update insert.sh

--- insert.sh
+++ insert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 make
-sudo rmmod -f balia1.ko
-sudo insmod balia1.ko
+sudo rmmod -f test.ko
+sudo insmod test.ko
 
-cc=$(< /proc/sys/net/ipv4/tcp_allowed_congestion_control)
-cc="$balia1"
-sudo sysctl net.ipv4.tcp_allowed_congestion_control=$cc
+cat /proc/sys/net/ipv4/tcp_allowed_congestion_control
+#cc="$balia1"
+#sudo sysctl net.ipv4.tcp_allowed_congestion_control=$cc
 
 #end
